7bd1b8e9844b23d0b845e97785913142e15e31d7
Visyi <simonbahrani@gmail.com>
2025-11-19 14:09:34 +0000

    commit

--- Interactive Shark Pixel Art/sketch.js
+++ Interactive Shark Pixel Art/sketch.js
@@ -1,7 +1,40 @@
+let sharkY = 0;
+let sharkX = 0;
+let sharkSpeed = 1;
+let SharyArray = [];
+
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1920, 1080);
+  frameRate(60)
+  SpawnShark();
 }
 
 function draw() {
   background(220);
+  cursor()
+  Shark();
+   
+
 }
+
+
+
+
+function Shark(){
+  ellipse(sharkX, sharkY, 50);
+  sharkX += sharkSpeed;
+
+  if (sharkX > width + 50){
+    SpawnShark();
+  }
+
+}
+
+function SpawnShark(){
+  sharkX = - 50;
+  sharkY = random(50, height - 50);
+}
+
+
